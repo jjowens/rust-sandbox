c15a5642ecb56c4728fc353a37230b4879b23101
= <=>
2025-04-07 10:50:36 +0100

    feat: added new author test

--- src/helpers/print_helper.rs
+++ src/helpers/print_helper.rs
@@ -1,4 +1,5 @@
 use crate::shop::book::Book;
+use crate::shop::author::Author;
 
 pub fn print_book(_book: Option<Book>) -> String {
     match _book {
@@ -7,6 +8,17 @@ pub fn print_book(_book: Option<Book>) -> String {
             item.title.to_string(),
             item.author.first_name.to_string(),
             item.author.last_name.to_string()
+        ),
+        None => "".to_string(),
+    }
+}
+
+pub fn print_author(_author: Option<Author>) -> String {
+    match _author {
+        Some(item) => format!(
+            "{} {}",
+            item.first_name.to_string(),
+            item.last_name.to_string()
         ),
         None => "".to_string(),
     }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use rust_sandbox::shop::{book::*, author::*};
 use rust_sandbox::helpers::print_helper::*;
 
 fn main() {
+    let _author = Author {
+        first_name: "Stephen".to_string(),
+        last_name: "King".to_string(),
+    };
+
     let _book1 = Book {
         title: "Salem's Lot".to_string(),
-        author: Author {
-            first_name: "Stephen".to_string(),
-            last_name: "King".to_string(),
-        },
+        author: _author,
     };
 
     println!("{}", print_book(Some(_book1)).to_string());
